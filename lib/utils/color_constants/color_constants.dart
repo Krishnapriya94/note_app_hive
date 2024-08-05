e2174163e1d77a5a22b9849feaace27f09bab224
Krishnapriya94 <kichuparvathi94@gmail.com>
2024-08-05 23:56:03 +0530

    updated note app

---
+++ lib/utils/color_constants/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color mainBlack = Color.fromARGB(255, 0, 0, 0);
+  static const Color mainRed = Color.fromARGB(255, 184, 7, 7);
+  static const Color mainGreen = Color.fromARGB(255, 3, 129, 9);
+}
